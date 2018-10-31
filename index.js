0a9ed76392b24e48f9209940671ba736afa20332
kelvv <kelvv@outlook.com>
2018-10-31 11:49:48 +0800

    add comment get

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let webHandler = require('./utils/webHandler')
+const Regex = require('regexper.js');
 
 let citys = [
   "Chicago",
@@ -12,54 +13,55 @@ let count=0
 
 async function work(city) {
   let page = 0
-  let businessResult = await webHandler.Get(`https://www.yelp.com/search/snippet?find_desc=&find_loc=Chicago`,null,null,true)
-  console.log('匹配')
-  console.log(Object.keys(businessResult))
 
-  let businessInfo = {
-    City: city,
-    Rest_Name: '',
-    Rest_Rate: 0,
-    Rest_total_Reviews: 0,
-    Rest_Price: 0,
-    Rest_location:'',
-    url: ''
-  }
+  while(true){
+    page++
+    console.log(`获取第${page}页，城市:${city}`)
+    let businessResult = await webHandler.Get(`https://www.yelp.com/search/snippet?find_desc=&find_loc=Chicago&start=${(page-1)*10}`,null,null,true)
 
-  if(businessResult.searchPageProps){
-    let result = businessResult.searchPageProps.searchMapProps.hovercardData
-    for(let key of Object.keys(result)){
-      let business = result[key]
-      businessInfo.url = business.businessUrl
-      businessInfo.Rest_Name = business.name
-      businessInfo.Rest_Rate = business.rating
-      businessInfo.Rest_total_Reviews = business.numReviews
-      businessInfo.Rest_location = business.addressLines[0]
-      console.log(businessInfo)
-    }
-  }else{
-    let result = businessResult.search_results
-    while(true){
-      let match = result.match(/href="([^"]+)"><span\s*>([^<]+)[\s\S]*?(\d*\.\d*)\s*star[\s\S]+?(\d+)\s*reviews[\s\S]+?address>\S*\s*([^<\n]+)\S*\s*/)
-      if(!match){
-        break
+    let businessInfos = []
+    if(businessResult.searchPageProps){
+      let result = businessResult.searchPageProps.searchMapProps.hovercardData
+      for(let key of Object.keys(result)){
+        let business = result[key]
+        let tTmp = {}
+        tTmp.url = business.businessUrl
+        tTmp.Rest_Name = business.name
+        tTmp.Rest_Rate = business.rating
+        tTmp.Rest_Price = 0
+        tTmp.Rest_total_Reviews = business.numReviews
+        tTmp.Rest_location = business.addressLines[0]
+        businessInfos.push(tTmp)
       }
-      businessInfo.url = match[1]
-      businessInfo.Rest_Name = match[2]
-      businessInfo.Rest_Rate = match[3]
-      businessInfo.Rest_total_Reviews = match[4]
-      businessInfo.Rest_location = match[5]
-      console.log(businessInfo)
+    }else{
+      let result = businessResult.search_results
+      let regex = new Regex(/href="([^"]+)"><span\s*>([^<]+)[\s\S]*?(\d*\.\d*)\s*star[\s\S]+?(\d+)\s*reviews[\s\S]+?address>\S*\s*([^<\n]+)\S*\s*/,'ig'); 
+      let matches = regex.matches(result)
+      for(let match of matches){
+        let tTmp = {}
+        tTmp.url = match[1]
+        tTmp.Rest_Name = match[2]
+        tTmp.Rest_Rate = match[3]
+        tTmp.Rest_Price = 0
+        tTmp.Rest_total_Reviews = match[4]
+        tTmp.Rest_location = match[5]
+        businessInfos.push(tTmp)
+      }
+    }
+
+    console.log(businessInfos)
+    console.log('根据商铺获取评论数据')
+    for(let businessInfo of businessInfos){
+      console.log(`https://www.yelp.com${businessInfo.url}`)
+      let businessInfoResult = await webHandler.Get(`https://www.yelp.com${businessInfo.url}`,null,null,false)
+      //评论列表
+      let regex = new Regex(/class="user-name"[\s\S]+?>([^<]+)[\s\S]+?<b>(\d+)[\s\S]+?<b>(\d+)<\/b>\s*reviews[\s\S]+?(\d+)<[\s\S]+?(\d+\.\d*)[\s\S]*?star rating[\s\S]+?<span class="rating-qualifier">\s*(\S+)[\s\S]+?<p\s*lang="en">([\s\S]+?)<\/p>[\s\S]+?Useful[\s\S+]?count">([^<]+)[\s\S]+?Funny[\s\S+]?count">([^<]+)[\s\S]+?Cool[\s\S+]?count">([^<]+)/,'i'); 
+      let matches = regex.matches(businessInfoResult)
+      console.log(matches[1])
+      console.log('写入excel')
     }
   }
 
-
-
-  /*
-  let businessResult = await webHandler.Get(`https://www.yelp.com/biz/girl-and-the-goat-chicago?start=20`,null,null,false)
-  console.log('匹配')
-  console.log(businessResult.match(/(Echoing) a lot of what I've/)[1])
-   */
 }
 
 async function begin(city) {
