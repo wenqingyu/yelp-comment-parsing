56be5b9dac052f806f4e22087e95f43633e526cc
kelvv <kelvv@outlook.com>
2018-11-04 14:36:54 +0800

    fix

--- cre.js
+++ cre.js
@@ -36,7 +36,7 @@ let preRequest = async function(options, done) {
 }
 
 var businessCraw = new Crawler({
-    maxConnections: 1,
+    maxConnections: 10,
     preRequest: async function(options, done) {
       requestCount++
       preRequest(options,done)
@@ -105,7 +105,7 @@ var businessCraw = new Crawler({
 });
 
 var commentCraw = new Crawler({
-    maxConnections: 1,
+    maxConnections: 10,
     preRequest: async function(options, done) {
       requestCount++
       preRequest(options,done)
