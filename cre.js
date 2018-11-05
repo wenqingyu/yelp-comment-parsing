ddb0a728af21e7e9b5edc72cb1db4d5946b9215a
kelvv <kelvv@outlook.com>
2018-11-05 17:45:27 +0800

    add timeout proxy

--- cre.js
+++ cre.js
@@ -25,7 +25,8 @@ let preRequest = async function(options, done) {
     requestCount++
     console.log(requestCount)
     options.proxy = db.get('proxy.url').value()
-    if(requestCount>=50||isRefresh){
+    
+    if(requestCount>=50||isRefresh||moment(db.get('proxy.time').value()).diff(moment(Date.now()),'minute')<= -5){
       requestCount=0
       isRefresh=false
       await webHandler.RefreshProxy()
